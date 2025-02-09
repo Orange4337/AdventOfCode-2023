7810d0ad9f0ac6ac5b1b700e173f029e871a9334
S.Oran <75443930+Orange4337@users.noreply.github.com>
2025-02-08 23:05:16 -0600

    finished day1 2024

--- 2024/HistorianHysteria.py
+++ 2024/HistorianHysteria.py
@@ -1,19 +1,19 @@
-text_file = open('day1input.txt', 'r')
+text_file = open('2024/day1input.txt', 'r')
 lines = text_file.readlines()
 left = []
 right = []
 
 for line in lines:
     pair = line.split()
-    left.append(pair[0])
-    right.append(pair[1])
+    left.append(int(pair[0]))
+    right.append(int(pair[1]))
 
 leftsorted = sorted(left)
 rightsorted = sorted(right)
 
 difference = []
 
-for i in range(leftsorted.size()):
+for i in range(0, len(leftsorted)):
     difference.append(abs(leftsorted[i]-rightsorted[i]))
 
 
@@ -21,4 +21,4 @@ sum = 0
 for d in difference:
     sum += d
 
-print(sum)
+print(f"\n sum: {sum}\n")
